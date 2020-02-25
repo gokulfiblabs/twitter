e3016e2360fbf1a975fada6c3892ab889bb1cf71
Gokul Raja T S <gokulraja.sunairaja@codingmart.com>
2020-02-25 19:26:31 +0530

    Comment added

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
     has_many :tweets
+    # Followers refers to the user followings
     has_many :follower_relationship, :class_name => 'Follow', :foreign_key => 'following_id', dependent: :destroy 
     has_many :followers, :through => :follower_relationship, :source => 'follower'
+    # Following refers to the user followers
     has_many :following_relationship, :class_name => 'Follow', :foreign_key => 'follower_id', dependent: :destroy 
     has_many :followings, :through => :following_relationship, :source => 'following'
 end
